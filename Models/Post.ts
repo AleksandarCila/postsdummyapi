e05b805bc999fa5703cd13834d55fd2329a09256
Aleksandar <aca96@live.co.uk>
2023-01-24 14:05:04 +0100

    Initialized and Models created

---
+++ Models/Post.ts
@@ -0,0 +1,12 @@
+import { UserPreview } from "./UserPreview";
+
+export type Post = {
+  id: string;
+  text: string;
+  image: string;
+  likes: number;
+  link: string;
+  tags: string[];
+  publishDate: string;
+  owner: UserPreview;
+};
